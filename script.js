d1cc827fb2f6afaa2db97c2b79d840f865de1a6c
scepticae <samuel_leonard@comcast.net>
2026-01-28 10:25:18 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// Fade in effect for web pages
+window.addEventListener("load", function () {
+  // Fade in after all resources (including images) are loaded
+  document.body.style.opacity = "1";
+});
+
+// Contact form submission handling
+(function () {
+  emailjs.init("kJaTMiVWo9ma7F1UJ");
+})();
+
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  emailjs.sendForm("service_v19j84p", "template_s0ka18a", this).then(
+    function () {
+      alert("Message sent!");
+      document.getElementById("contactForm").reset();
+    },
+    function (error) {
+      alert("Failed to send message");
+      console.error(error);
+    }
+  );
+});
