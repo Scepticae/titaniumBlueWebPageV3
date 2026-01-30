c82f76082beea1425e8f7a0c56df9bd767c92d03
scepticae <samuel_leonard@comcast.net>
2026-01-30 16:04:42 -0500

    More Responsive Changes

--- script.js
+++ script.js
@@ -4,30 +4,11 @@ window.addEventListener("load", function () {
   document.body.style.opacity = "1";
 });
 
-// Contact form submission handling
-(function () {
-  emailjs.init("kJaTMiVWo9ma7F1UJ");
-})();
-
-document.getElementById("contactForm").addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  emailjs.sendForm("service_v19j84p", "template_s0ka18a", this).then(
-    function () {
-      alert("Message sent!");
-      document.getElementById("contactForm").reset();
-    },
-    function (error) {
-      alert("Failed to send message");
-      console.error(error);
-    }
-  );
-});
-
 // Hamburger Button Action
 const hamburgerBtn = document.getElementById("hamburger");
 const openNav = document.getElementById("open");
 
 hamburgerBtn.addEventListener("click", () => {
+  console.log("clicked");
   openNav.classList.toggle("open");
 });
